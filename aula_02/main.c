0296c1f1a9e900d1087f7df0c44fde312a2c71e5
jhgborges <jhgborges@gmail.com>
2022-09-21 20:06:54 -0300

    Loops e números pares

--- aula_02/main.c
+++ aula_02/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main() {
-    int cont;
-    printf("Pares (10 e 100): for\n");
-    for(cont=8; cont<=98; cont=cont+2){
-        printf("Valores (controle | exibição): %d e %d\n", cont,cont+2);
-    }
+int main () {
+    int x = 10;
+    printf("Pares (10/100): do/while\n");
+    do {
+        printf("Valor de x: %d\n", x);
+        x = x + 2;
+    }while( x <= 100 );
     return 0;
 }
